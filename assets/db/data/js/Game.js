0a5caa5cffeab1cea9890c9f350ae5e77d8533fa
sfinks <urasfinks@yandex.ru>
2023-06-27 23:27:30 +0300

    Управле

--- assets/db/data/js/Game.js
+++ assets/db/data/js/Game.js
@@ -3,27 +3,48 @@ if (bridge.args["switch"] == "constructor") {
 }
 
 if (bridge.args["switch"] == "onChange") {
-    var data = bridge.args["subscribe"]["data"];
-    bridge.log(data);
+    var socketData = bridge.args["subscribe"]["data"];
+    bridge.log(socketData);
     var newStateData = {};
 
-    if (data["user" + bridge.unique] == undefined || data["user" + bridge.unique]["name"] == "") {
+    if (socketData["user" + bridge.unique] == undefined || socketData["user" + bridge.unique]["name"] == "") {
         newStateData["SwitchKey"] = "default";
     }
-    if (data["user" + bridge.unique] != undefined) {
-        newStateData["deviceName"] = data["user" + bridge.unique]["name"];
+    if (socketData["user" + bridge.unique] != undefined) {
+        newStateData["deviceName"] = socketData["user" + bridge.unique]["name"];
     }
+    newStateData["listPerson"] = getListPerson(socketData);
+    newStateData["originSocketData"] = socketData;
+
     bridge.call('SetStateData', {
         "map": newStateData
     });
 }
 
+if (bridge.args["switch"] == "addPerson") {
+    if (bridge.state["Name"] != undefined && bridge.state["Name"].trim() != "") {
+        var data = {};
+        userBlock = {
+            name: bridge.state["Name"]
+        };
+        data["user" + bridge.call('Uuid', {})["uuid"]] = userBlock;
+        socketSave(data);
+        bridge.call("NavigatorPop", {});
+    } else {
+        bridge.alert("Имя слишком пустое)");
+    }
+}
+
 if (bridge.args["switch"] == "setName") {
     //bridge.log();
     var data = {};
-    data["user" + bridge.unique] = {
-        "name": bridge.state["Name"]
+    var userBlock = {};
+    var socketData = bridge.state["originSocketData"];
+    if (socketData["user" + bridge.unique] != undefined) {
+        userBlock = socketData["user" + bridge.unique];
     }
+    userBlock["name"] = bridge.state["Name"];
+    data["user" + bridge.unique] = userBlock;
     socketSave(data);
 }
 
@@ -35,4 +56,45 @@ function socketSave(data) {
         "uuid": socketUuid,
         "value": data
     });
+}
+
+function getListPerson(socketData) {
+    var result = [];
+    var count = 1;
+    for (var key in socketData) {
+        if (key.startsWith("user") && key != ("user" + bridge.unique)) {
+            result.push({
+                label: socketData[key]["name"],
+                templateWidgetSrc: "ButtonIcon",
+                iconSrc: "edit"
+            });
+        }
+    }
+    return result;
+}
+
+if (bridge.args["switch"] == "onRenderFloatingActionButton") {
+    if (bridge.state["originSocketData"] != undefined && bridge.state["originSocketData"]["owner"] == bridge.unique) {
+        bridge.call("DataSourceSet", {
+            "uuid": "FloatingActionButton.json",
+            "type": "virtual",
+            "value": {
+                "flutterType": "FloatingActionButton",
+                "child": {
+                    "flutterType": "Icon",
+                    "src": "add"
+                },
+                "onPressed": {
+                    "sysInvoke": "NavigatorPush",
+                    "args": {
+                        "subscribeOnChangeUuid": bridge.pageArgs["subscribeOnChangeUuid"],
+                        "type": "BottomSheet",
+                        "link": {
+                            "template": "GameAddPerson.json",
+                        }
+                    }
+                }
+            }
+        });
+    }
 }
