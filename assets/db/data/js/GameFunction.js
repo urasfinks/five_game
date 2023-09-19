f0efb2bf4fce29650c6934f37d867e3c1cb5b875
sfinks <urasfinks@yandex.ru>
2023-09-19 23:05:58 +0300

    Рефактор главной страницы + import GameFunction.js

---
+++ assets/db/data/js/GameFunction.js
@@ -0,0 +1,372 @@
+bridge.log("GameFunction.js included");
+
+function isMyGame(socketData) {
+    return (socketData["gameState"] === "run" && socketData["runTeam"] === getMyTeam(socketData));
+}
+
+function isOwner(socketData) {
+    return socketData !== undefined && socketData["owner"] === bridge.unique;
+}
+
+function isCaptain(socketData) {
+    return socketData !== undefined && socketData["user" + bridge.unique] !== undefined && socketData["user" + bridge.unique]["role"] === "captain";
+}
+
+function getMyTeam(socketData) {
+    return socketData["user" + bridge.unique] !== undefined ? socketData["user" + bridge.unique]["team"] : "watcher";
+}
+
+function getGridWord(socketData) {
+    var listCard = [];
+    for (var key in socketData) {
+        if (key.startsWith("card")) {
+            socketData[key]["index"] = key.split("card")[1];
+            listCard.push(socketData[key]);
+        }
+    }
+    var canBePressed = isMyGame(socketData);
+    var isCapt = isCaptain(socketData);
+
+    listCard.sort(function (a, b) {
+        return a["index"] - b["index"];
+    });
+    var counter = 0;
+    var column = {
+        "flutterType": "Column",
+        "children": []
+    };
+    var matrix = bridge.orientation === "portrait" ? {col: 4, row: 7} : {col: 7, row: 4};
+    var colorCard = {
+        "red": "red",
+        "blue": "blue",
+        "neutral": "#efd9b9",
+        "die": "black"
+    };
+    var colorText = {
+        "red": "white",
+        "blue": "white",
+        "neutral": "black",
+        "die": "white"
+    };
+    var mapCount = {};
+    var mapCountMy = {};
+    for (var key in socketData) {
+        if (key.startsWith("tapCard_")) {
+            var curKey = "i" + key.split("tapCard_")[1].split("_")[0];
+            var amI = key.split("tapCard_")[1].split("_")[1];
+            if (mapCount[curKey] === undefined) {
+                mapCount[curKey] = 1;
+            } else {
+                mapCount[curKey]++;
+            }
+            if (amI === bridge.unique) {
+                mapCountMy[curKey] = true;
+            }
+        }
+    }
+    for (var c = 0; c < matrix.row; c++) {
+        var row = {
+            "flutterType": "Row",
+            "children": []
+        };
+        for (var i = 0; i < matrix.col; i++) {
+            var curIndex = counter++;
+            var cardData = listCard[curIndex];
+            var onTap = (canBePressed && cardData["selected"] == null) ? {
+                "jsInvoke": "Game.js",
+                "args": {
+                    "includeAll": true,
+                    "switch": "onCardTap",
+                    "index": counter - 1
+                }
+            } : null;
+            if (cardData !== undefined) {
+                var tapCount = mapCount["i" + curIndex] !== undefined ? {
+                    "flutterType": "Container",
+                    "width": 20,
+                    "height": 20,
+                    "decoration": {
+                        "flutterType": "BoxDecoration",
+                        "color": mapCountMy["i" + curIndex] === undefined ? "rgba:0,0,0,0.75" : "rgba:50,205,50,0.75",
+                        "borderRadius": 9,
+                        "border": {
+                            "flutterType": "Border",
+                            "color": "white",
+                            "width": 1.3
+                        }
+                    },
+                    "child": {
+                        "flutterType": "Center",
+                        "child": {
+                            "flutterType": "Text",
+                            "label": "" + mapCount["i" + curIndex],
+                            "style": {
+                                "flutterType": "TextStyle",
+                                "fontSize": 10,
+                                "color": "white"
+                            }
+                        }
+                    }
+                } : {
+                    "flutterType": "SizedBox"
+                };
+                if (cardData["selected"] != null) {
+                    tapCount = {
+                        "flutterType": "Container",
+                        "width": 10,
+                        "height": 10,
+                        "decoration": {
+                            "flutterType": "BoxDecoration",
+                            "color": cardData["selected"],
+                            "borderRadius": 9
+                        },
+                        "child": {
+                            "flutterType": "SizedBox"
+                        }
+                    };
+                }
+                //Если карточка выбрана - показываем её реальный цвет
+                var curColorCard = (isCapt || cardData["selected"] != null) ? colorCard[cardData["team"]] : "#efd9b9";
+                var curColorText = (isCapt || cardData["selected"] != null) ? colorText[cardData["team"]] : "black";
+                var block = {
+                    "flutterType": "Container",
+                    "height": 50,
+                    "margin": 5,
+                    "child": {
+                        "flutterType": "Material",
+                        "color": curColorCard,
+                        "borderRadius": 5,
+                        "child": {
+                            "flutterType": "InkWell",
+                            "onTap": onTap,
+                            "child": {
+                                "flutterType": "Stack",
+                                "alignment": "topEnd",
+                                "children": [
+                                    {
+                                        "flutterType": "Center",
+                                        "child": {
+                                            "flutterType": "Text",
+                                            "textAlign": "center",
+                                            "label": cardData["selected"] == null ? cardData["label"].toUpperCase() : "",
+                                            "style": {
+                                                "flutterType": "TextStyle",
+                                                "fontSize": 12,
+                                                "fontWeight": "bold",
+                                                "color": curColorText
+                                            }
+                                        }
+                                    },
+                                    tapCount
+                                ]
+                            }
+                        }
+                    }
+                };
+                row["children"].push({
+                    "flutterType": "Expanded",
+                    "child": block
+                });
+            }
+        }
+        column["children"].push(row);
+    }
+    return column;
+}
+
+function onRenderFloatingActionButton(socketData, socketUuid) {
+    if (isOwner(socketData) && socketData["gameState"] === "team") {
+        bridge.call("Show", {
+            "case": "actionButton",
+            "template": {
+                "flutterType": "FloatingActionButton",
+                "child": {
+                    "flutterType": "Icon",
+                    "src": "add"
+                },
+                "onPressed": {
+                    "sysInvoke": "NavigatorPush",
+                    "args": {
+                        "socketUuid": socketUuid,
+                        "type": "bottomSheet",
+                        "height": 200,
+                        "link": {
+                            "template": "GamePersonAdd.json",
+                        }
+                    }
+                }
+            }
+        });
+    } else {
+        bridge.call("Hide", {
+            "case": "actionButton"
+        });
+    }
+}
+
+function getUsers(socketData) {
+    var reds = [], blues = [];
+    for (var key in socketData) {
+        if (key.startsWith("user") && socketData[key]["role"] === "captain") {
+            if (socketData[key]["team"] === "red") {
+                reds.push(socketData[key]["name"] + " (Капитан)");
+            } else if (socketData[key]["team"] === "blue") {
+                blues.push(socketData[key]["name"] + " (Капитан)");
+            }
+        }
+    }
+    for (var key in socketData) {
+        if (key.startsWith("user") && socketData[key]["role"] !== "captain") {
+            if (socketData[key]["team"] === "red") {
+                reds.push(socketData[key]["name"]);
+            } else if (socketData[key]["team"] === "blue") {
+                blues.push(socketData[key]["name"]);
+            }
+        }
+    }
+    return {
+        "red": reds.join(", "),
+        "blue": blues.join(", "),
+    }
+}
+
+function calculateScore(socketData, newStateData, socketUuid) {
+    var blue = 0, red = 0, all = 0;
+    for (var key in socketData) {
+        if (key.startsWith("card")) {
+            if (["red", "blue"].includes(socketData[key]["team"])) {
+                all++;
+            }
+            if (socketData[key]["selected"] !== undefined) {
+                if (socketData[key]["team"] === "blue") {
+                    blue++;
+                } else if (socketData[key]["team"] === "red") {
+                    red++;
+                }
+            }
+        }
+    }
+    if (socketData["description"] === undefined) {
+        if (blue + red === all) {
+            socketSave({
+                description: "Победила команда " + (blue > red ? "синих" : "красных"),
+                gameState: "finish"
+            }, socketUuid);
+        }
+    }
+    newStateData["scoreRed"] = red;
+    newStateData["scoreBlue"] = blue;
+    newStateData["scoreAll"] = all;
+}
+
+function socketSave(data, socketUuid) {
+    bridge.call("DataSourceSet", {
+        //"debugTransaction": true,
+        "type": "socket",
+        "uuid": socketUuid,
+        "value": data
+    });
+}
+
+function getListPerson(socketData) {
+    var listPerson = [];
+    for (var key in socketData) {
+        if (key.startsWith("user")) {
+            socketData[key]["id"] = key;
+            listPerson.push(socketData[key]);
+        }
+    }
+    return listPerson;
+}
+
+function getListPersonGroup(socketData, team, socketUuid) {
+    var result = [];
+    var isOwn = isOwner(socketData);
+    var listPerson = getListPerson(socketData);
+    listPerson.sort(function (a, b) {
+        return a["role"] === "captain" ? -1 : 1;
+    });
+    for (var i = 0; i < listPerson.length; i++) {
+        var curTeam = ["red", "blue"].includes(listPerson[i]["team"]) ? listPerson[i]["team"] : "undefined";
+        if (curTeam === team) {
+            var ami = listPerson[i]["id"] === ("user" + bridge.unique) ? "(Я) " : "";
+            var isCaptain = listPerson[i]["role"] === "captain" ? "(Капитан) " : "";
+            var background = "schema:onBackground";
+            if (team === "red") {
+                background = "red.600";
+            }
+            if (team === "blue") {
+                background = "blue.600";
+            }
+            var iconColor = (team === "red" || team === "blue") ? "#ffffff" : "#999999";
+
+            var onTap = isOwn ? {
+                "sysInvoke": "NavigatorPush",
+                "args": {
+                    "socketUuid": socketUuid,
+                    "personKey": listPerson[i]["id"],
+                    "personValue": listPerson[i],
+                    "type": "bottomSheet",
+                    "height": 400,
+                    "link": {
+                        "template": "GamePersonEdit.json",
+                    }
+                }
+            } : {};
+
+            result.push({
+                "label": ami + isCaptain + listPerson[i]["name"],
+                "templateWidgetSrc": "IteratorButtonIcon",
+                "iconSrc": isOwn ? "edit" : "person_outline",
+                "background": background,
+                "iconColor": iconColor,
+                "onTap": onTap
+            });
+        }
+    }
+    return result;
+}
+
+function getFlagToWordGameState(socketData) {
+    if (!isOwner(socketData)) {
+        return "false";
+    }
+
+    var listPerson = getListPerson(socketData);
+    var countCaptain = 0;
+    var countRed = 0;
+    var countBlue = 0;
+    var countUndefined = 0;
+
+    for (var i = 0; i < listPerson.length; i++) {
+        if (listPerson[i]["role"] === "captain") {
+            countCaptain++;
+            if (listPerson[i]["team"] === "red") {
+                countRed++;
+            }
+            if (listPerson[i]["team"] === "blue") {
+                countBlue++;
+            }
+        }
+
+        if (listPerson[i]["team"] === undefined || listPerson[i]["team"] === "undefined") {
+            countUndefined++;
+        }
+    }
+
+    if (countCaptain !== 2) {
+        if (listPerson.length > 1) {
+            bridge.alert("Надо выбрать капитанов для двух команд");
+        }
+        return "false";
+    }
+    if (countUndefined > 0) {
+        bridge.alert("Надо распределить " + countUndefined + " человек по командам");
+        return "false";
+    }
+    if (countRed > 1 || countBlue > 1) {
+        bridge.alert("У команды должен быть 1 капитан");
+        return "false";
+    }
+    return "true";
+}
---
+++ assets/db/data/js/HomePage.js
@@ -0,0 +1,158 @@
+if (bridge.args["switch"] === "constructor") {
+
+    bridge.asyncImport("HomePageFunction.js", {});
+    bridge.asyncImport("GameFunction.js", {});
+
+    bridge.call("DbQuery", {
+        "sql": "select * from data where key_data = ? and is_remove_data = 0 order by id_data desc",
+        "args": ["Game"],
+        "onFetch": {
+            "jsInvoke": "HomePage.js",
+            "args": {
+                "includeAll": true,
+                "switch": "selectMyGame"
+            }
+        }
+    });
+}
+
+if (bridge.args["switch"] === "selectMyGame") {
+    var countGameNotFinish = 0;
+    if (bridge.args["fetchDb"].length > 0) {
+        for (var i = 0; i < bridge.args["fetchDb"].length; i++) {
+            if (bridge.args["fetchDb"][i]["value_data"]["gameState"] !== "finish") {
+                countGameNotFinish++;
+            }
+        }
+        bridge.call("SetStateData", {
+            "map": {
+                "countGameNotFinish": countGameNotFinish == 0 ? "" : countGameNotFinish
+            }
+        });
+    }
+}
+
+if (bridge.args["switch"] === "createGame") {
+    var uuid = bridge.call("Util", {"case": "uuid"})["uuid"];
+    gameData = {
+        "description": "CodeNames",
+        "gameUuid": uuid,
+        "owner": bridge.unique,
+        "runTeam": "red",
+        "gameState": "team", //team/word/run/finish
+    };
+    gameData["user" + bridge.unique] = {
+        "name": bridge.call("GetStorage", {"key": "accountName", "default": ""})["accountName"],
+        "team": "",
+        "role": "player"
+    };
+    bridge.call("DataSourceSet", {
+        // Хак, для сокетных данных, которые необходимо заинсертит в локальную БД
+        // В стандартном случае посылается diff на сервер, а нам этого не надо
+        // Таким образом новая сокетная запись при синхронизации уедет на сервер
+        // Иначе мы будем получать ошибку, что НЕТ ДОСТУПА, так как данного uuid нет в удалённой БД
+        "beforeSync": true,
+        "updateIfExist": false,
+        "uuid": uuid,
+        "value": gameData,
+        "parent": null,
+        "type": "socket",
+        "debugTransaction": true,
+        "key": "Game",
+        "onPersist": {
+            "jsInvoke": "HomePage.js",
+            "args": {
+                "includeAll": true,
+                "gameUuid": uuid,
+                "switch": "gameSaveToDb"
+            }
+        }
+    });
+}
+
+if (bridge.args["switch"] === "gameSaveToDb") {
+    bridge.call("NavigatorPush", getNavigatorPushGameArgs(bridge.args["gameUuid"], bridge.args["gameUuid"]));
+}
+
+if (bridge.args["switch"] === "requestConfirmCode") {
+    if (bridge.state["main"]["CodeValue"] === undefined || bridge.state["main"]["CodeValue"].trim() === "") {
+        bridge.alert("Код слишком пустой)");
+    } else {
+        bridge.call("Http", {
+            "uri": "/GetCodeUuid",
+            "body": {
+                "code": bridge.state["main"]["CodeValue"] * 1,
+            },
+            "onResponse": {
+                "jsInvoke": "HomePage.js",
+                "args": {
+                    "includeAll": true,
+                    "switch": "onConfirmCodeResponse"
+                }
+            }
+        });
+    }
+}
+
+if (bridge.args["switch"] === "onConfirmCodeResponse") {
+    if (bridge.checkHttpResponse([{
+        error: "$.code: must have a minimum value of 100000",
+        display: "Код должен быть 6 знаков"
+    }])) {
+        var socketUuid = bridge.args["httpResponse"]["data"]["data"]["uuid"];
+        bridge.call("DbQuery", {
+            "sql": "select * from data where uuid_data = ? or parent_uuid_data = ? order by id_data desc",
+            "args": [socketUuid, socketUuid],
+            "onFetch": {
+                "jsInvoke": "HomePage.js",
+                "args": {
+                    "socketUuid": socketUuid,
+                    "includeAll": true,
+                    "switch": "selectAllReadyGame"
+                }
+            }
+        });
+    }
+}
+
+if (bridge.args["switch"] === "selectAllReadyGame") {
+    var socketUuid = bridge.args["socketUuid"];
+    if (bridge.args["fetchDb"].length > 0) {
+        var fetch = bridge.args["fetchDb"][0];
+        closeThisPageAndOpenGame(socketUuid, fetch["uuid_data"]);
+    } else {
+        var gameUuid = bridge.call("Util", {"case": "uuid"})["uuid"];
+        bridge.call("DataSourceSet", {
+            // Хак, для сокетных данных, которые необходимо заинсертит в локальную БД
+            // В стандартном случае посылается diff на сервер, а нам этого не надо
+            // Таким образом новая сокетная запись при синхронизации уедет на сервер
+            // Иначе мы будем получать ошибку, что НЕТ ДОСТУПА, так как данного uuid нет в удалённой БД
+            "beforeSync": true,
+            "updateIfExist": false,
+            "uuid": gameUuid,
+            "value": {},
+            "parent": socketUuid,
+            "type": "socket",
+            "debugTransaction": true,
+            "key": "Game",
+            "onPersist": {
+                "jsInvoke": "HomePage.js",
+                "args": {
+                    "includeAll": true,
+                    "socketUuid": socketUuid,
+                    "gameUuid": gameUuid,
+                    "switch": "gameConnectSaveToDb"
+                }
+            }
+        });
+    }
+}
+
+if (bridge.args["switch"] === "gameConnectSaveToDb") {
+    closeThisPageAndOpenGame(bridge.args["socketUuid"], bridge.args["gameUuid"]);
+}
+
+function closeThisPageAndOpenGame(socketUuid, gameUuid) {
+    bridge.call("NavigatorPop", {});
+    bridge.call("NavigatorPush", getNavigatorPushGameArgs(gameUuid, socketUuid));
+}
