7423052c4e849c56f357e4039f9b495cc548ef6c
sfinks <urasfinks@yandex.ru>
2023-10-09 01:57:55 +0300

    First deploy

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:rjdu/rjdu.dart';
 void main() async {
   GlobalSettings().setHost("https://5gm.ru");
   GlobalSettings().setWs("wss://5gm.ru");
+  //GlobalSettings().setHost("http://192.168.0.15");
+  //GlobalSettings().setWs("ws://192.168.0.15");
   RjDu.init();
 
   runApp(await RjDu.runApp());
