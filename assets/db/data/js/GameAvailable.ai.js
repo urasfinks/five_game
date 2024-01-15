dc5faf889de69c9e53205206f01258ab29004c28
sfinks <urasfinks@yandex.ru>
2024-01-15 22:17:04 +0300

    Миграция jsRouter

--- assets/db/data/js/GameAvailable.ai.js
+++ assets/db/data/js/GameAvailable.ai.js
@@ -1,33 +1,36 @@
-if (bridge.args["switch"] === "constructor") {
-    var state = [];
-    state.push({
-        "templateCustom": "templateGameLine",
-        "label1": "Тайные Связи",
-        "label2": "Битва умов",
-        "src1": "sc2.png",
-        "src2": "bom5.png",
-        "childKey1": "" ,
-        "childKey2": "",
-        "onTap1": {
-            "jsInvoke": "HomePage.js",
-            "args": {
-                "game": "SecretConnections",
-                "includeAll": true,
-                "switch": "createGame"
+function GameAvailableRouter(){
+    this.constructor = function(){
+        var state = [];
+        state.push({
+            "templateCustom": "templateGameLine",
+            "label1": "Тайные Связи",
+            "label2": "Битва умов",
+            "src1": "sc2.png",
+            "src2": "bom5.png",
+            "childKey1": "" ,
+            "childKey2": "",
+            "onTap1": {
+                "jsInvoke": "HomePage.js",
+                "args": {
+                    "game": "SecretConnections",
+                    "includeAll": true,
+                    "switch": "createGame"
+                }
+            },
+            "onTap2": {
+                "jsInvoke": "HomePage.js",
+                "args": {
+                    "game": "BattleOfMinds",
+                    "includeAll": true,
+                    "switch": "createGame"
+                }
             }
-        },
-        "onTap2": {
-            "jsInvoke": "HomePage.js",
-            "args": {
-                "game": "BattleOfMinds",
-                "includeAll": true,
-                "switch": "createGame"
+        });
+        bridge.call("SetStateData", {
+            "map": {
+                "listGame": state
             }
-        }
-    });
-    bridge.call("SetStateData", {
-        "map": {
-            "listGame": state
-        }
-    });
+        });
+    }
 }
+bridge.addRouter(new GameAvailableRouter());
