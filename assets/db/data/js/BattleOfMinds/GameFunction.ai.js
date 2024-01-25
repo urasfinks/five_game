82a90df6be52b8f37a40cdf0601c5299dd56f4c6
sfinks <urasfinks@yandex.ru>
2024-01-25 23:54:41 +0300

    Группы

--- assets/db/data/js/BattleOfMinds/GameFunction.ai.js
+++ assets/db/data/js/BattleOfMinds/GameFunction.ai.js
@@ -1,5 +1,7 @@
 bridge.global.BattleOfMinds = {
 
+    undefinedGroup: "Не в группе",
+
     calculateScore: function (socketData, newStateData, socketUuid) {
         // newStateData["scoreRed"] = red;
         // newStateData["scoreBlue"] = blue;
@@ -9,76 +11,72 @@ bridge.global.BattleOfMinds = {
 
     getListPersonGroup: function (socketData, socketUuid) {
         var result = [];
-        // var isOwn = isOwner(socketData);
-        // var listPerson = getListPerson(socketData);
-        // var alreadyPersonGroup = [];
-        // for (var i = 0; i < listPerson.length; i++) {
-        //     alreadyPersonGroup.push(listPerson[i]["team"]);
-        // }
-        // alreadyPersonGroup = bridge.arrayUnique(alreadyPersonGroup);
-        // var resultTeam = [];
-        // for (var i = 0; i < alreadyPersonGroup.length; i++) {
-        //     if (alreadyPersonGroup[i].trim() !== "") {
-        //         resultTeam.push({
-        //             "label": alreadyPersonGroup[i]
-        //         });
-        //     }
-        // }
-        //
-        // listPerson.sort(function (a, b) {
-        //     return a["role"] === "captain" ? -1 : 1;
-        // });
-        //
-        // for (var i = 0; i < listPerson.length; i++) {
-        //     var curTeam = ["red", "blue"].includes(listPerson[i]["team"]) ? listPerson[i]["team"] : "undefined";
-        //     if (curTeam === team) {
-        //         var ami = listPerson[i]["id"] === bridge.unique ? "Я " : "";
-        //         var isCaptain = listPerson[i]["role"] === "captain" ? "Капитан " : "";
-        //         var background = "schema:onBackground";
-        //         var textColor = "schema:inversePrimary";
-        //         if (team === "red") {
-        //             background = "red.600";
-        //             textColor = "white";
-        //         }
-        //         if (team === "blue") {
-        //             background = "blue.600";
-        //             textColor = "white";
-        //         }
-        //         var iconColor = (team === "red" || team === "blue") ? "#ffffff" : "#999999";
-        //
-        //         var onTap = isOwn ? {
-        //             "sysInvoke": "NavigatorPush",
-        //             "args": {
-        //                 "socketUuid": socketUuid,
-        //                 "personId": listPerson[i]["id"],
-        //                 "personValue": listPerson[i],
-        //                 "listOptions": resultTeam,
-        //                 "type": "bottomSheet",
-        //                 "height": 360,
-        //                 "link": {
-        //                     "template": "BattleOfMinds/GamePersonEdit.json",
-        //                 }
-        //             }
-        //         } : {};
-        //         var iconPerson = "person_outline";
-        //         var name = listPerson[i]["name"];
-        //         if (listPerson[i]["static"] != undefined && listPerson[i]["static"] === true) {
-        //             name = "[" + name + "]";
-        //             iconPerson = "android";
-        //         }
-        //         var iconSrc = isOwn ? "edit" : iconPerson;
-        //         result.push({
-        //             "label": ami + isCaptain + name,
-        //             "templateWidgetSrc": "IteratorButtonIcon",
-        //             "iconSrc": iconSrc,
-        //             "background": background,
-        //             "textColor": textColor,
-        //             "iconColor": iconColor,
-        //             "onTap": onTap
-        //         });
-        //     }
-        // }
+        var isOwn = isOwner(socketData);
+        var listPerson = getListPerson(socketData);
+        var alreadyPersonGroup = [];
+        for (var i = 0; i < listPerson.length; i++) {
+            if (listPerson[i]["team"].trim() !== "") {
+                alreadyPersonGroup.push(listPerson[i]["team"]);
+            } else {
+                alreadyPersonGroup.push(this.undefinedGroup);
+            }
+        }
+        alreadyPersonGroup = bridge.arrayUnique(alreadyPersonGroup);
+        var resultTeam = [];
+        for (var i = 0; i < alreadyPersonGroup.length; i++) {
+            resultTeam.push({
+                "label": alreadyPersonGroup[i]
+            });
+        }
+        for (var i = 0; i < alreadyPersonGroup.length; i++) {
+            result.push({
+                "templateCustom": "groupName",
+                "label": alreadyPersonGroup[i]
+            });
+            this.userByGroup(listPerson, result, alreadyPersonGroup[i], isOwn, socketUuid, resultTeam);
+        }
         return result;
+    },
+
+    userByGroup: function (listPerson, result, curTeam, isOwn, socketUuid, resultTeam) {
+        for (var i = 0; i < listPerson.length; i++) {
+            if (
+                listPerson[i]["team"] === curTeam
+                || (listPerson[i]["team"].trim() === "" && this.undefinedGroup === curTeam)
+            ) {
+                var ami = listPerson[i]["id"] === bridge.unique ? "Я " : "";
+
+                var onTap = isOwn ? {
+                    "sysInvoke": "NavigatorPush",
+                    "args": {
+                        "socketUuid": socketUuid,
+                        "personId": listPerson[i]["id"],
+                        "personValue": listPerson[i],
+                        "listOptions": resultTeam,
+                        "type": "bottomSheet",
+                        "height": 360,
+                        "link": {
+                            "template": "BattleOfMinds/GamePersonEdit.json",
+                        }
+                    }
+                } : {};
+                var iconPerson = "person_outline";
+                var name = listPerson[i]["name"];
+                if (listPerson[i]["static"] != undefined && listPerson[i]["static"] === true) {
+                    name = "[" + name + "]";
+                    iconPerson = "android";
+                }
+                var iconSrc = isOwn ? "edit" : iconPerson;
+                result.push({
+                    "label": ami + name,
+                    "templateCustom": "user",
+                    "templateWidgetSrc": "IteratorButtonIcon",
+                    "iconSrc": iconSrc,
+                    "iconColor": "#999999",
+                    "onTap": onTap
+                });
+            }
+        }
     },
 
     getFlagToWordGameState: function (socketData, state) {
